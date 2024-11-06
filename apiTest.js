426d0a5511aac859a2a1ae6c00240eeae2d6c71d
labroudimariam <labroudimariam@gmail.com>
2024-11-06 22:01:57 +0100

    commit

--- apiTest.js
+++ apiTest.js
@@ -1,54 +1,82 @@
 async function getWeatherData(coords) {
-    const url = `https://api.open-meteo.com/v1/forecast?latitude=${coords[0]}&longitude=${coords[1]}&current_weather=true&daily=temperature_2m_max,temperature_2m_min,precipitation_sum&timezone=auto`;
-    
-    try {
-        let resp = await axios.get(url);
-        let data = resp.data;
-        console.log(data);
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${coords[0]}&longitude=${coords[1]}&current=temperature_2m,is_day,rain,cloud_cover,wind_speed_10m&daily=temperature_2m_max,temperature_2m_min,rain_sum&timezone=auto`;
 
-        let temperature = data.current_weather.temperature;
-        
-        $(".temperature").text(`${temperature} °C`);
-        $(".description").text(data.current_weather.weathercode === 0 ? "Clear" : "Cloudy");
 
+    let resp = await axios.get(url);
+    let data = resp.data;
+    console.log(data);
+
+    let is_day = data.current.is_day;
+
+    if (is_day === 0) {
+        $("body").css("background-image", "url('bg2.jpg')")
         let imgWeather = "";
-        if(temperature>25){
-            imgWeather = "sun2.png"
+        if (data.current.rain > 0) {
+            imgWeather = "rain-cloud.png"
         }
-        else{
-            imgWeather = "cloudy.png"
+        else if (data.current.cloud_cover != 0) {
+            imgWeather = "cloudy-night.png"
+        } else {
+            imgWeather = "moon1.png"
         }
+
         $(".img_weather").html(`<img width="150px" src="${imgWeather}" alt="">`);
 
-        let cityName = $("#cities option:selected").text();
-        $(".city_name").text(cityName);
-        $(".date").text(new Date().toLocaleDateString());
+    } else {
+        $("body").css("background-image", "url('bg1.jpg')")
+        let imgWeather = "";
+        if (data.current.rain > 0) {
+            imgWeather = "rain-cloud.png"
+        }
+        else if (data.current.cloud_cover != 0) {
+            imgWeather = "cloudy.png"
+        } else {
+            imgWeather = "sun2.png"
+        }
 
-    
-        $(".week_weather .days").each((index, element) => {
-            let day = new Date(data.daily.time[index]).toLocaleString('en', { weekday: 'long' });
-            let maxTemp = data.daily.temperature_2m_max[index];
-            let minTemp = data.daily.temperature_2m_min[index];
-
-            let iconSrc = "";
-            if(maxTemp<25 && minTemp<15){
-                iconSrc = "cloudy.png"
-            }
-            else{
-                iconSrc = "sun2.png"
-            }
-
-            $(element).find(".day").text(day);
-            $(element).find(".day_temp").text(`${maxTemp}°C / ${minTemp}°C`);
-            $(element).find(".day_img").html(`<img width="50px" src="${iconSrc}" alt="">`);
-        });
-
-    } catch (error) {
-        console.error("Error fetching weather data:", error);
+        $(".img_weather").html(`<img width="150px" src="${imgWeather}" alt="">`);
     }
+
+    let temperature = data.current.temperature_2m;
+
+    $(".temperature").text(`${temperature} °C`);
+    $(".description").text(
+        data.current.rain > 0 ? "Rainy" :
+            data.current.cloud_cover === 0 ? "Clear" : "Cloudy"
+    );
+
+
+
+    let cityName = $("#cities option:selected").text();
+    $(".city_name").text(cityName);
+    $(".date").text(new Date().toLocaleDateString());
+
+
+    $(".week_weather .days").each((index, element) => {
+        let day = new Date(data.daily.time[index]).toLocaleString('en', { weekday: 'long' });
+        let maxTemp = data.daily.temperature_2m_max[index];
+        let minTemp = data.daily.temperature_2m_min[index];
+        let rainSum = data.daily.rain_sum[index];
+
+        let iconSrc = "";
+        if (rainSum > 0) {
+            iconSrc = "rain-cloud.png"
+        }
+        else if (maxTemp < 20) {
+            iconSrc = "cloudy.png"
+        }
+        else {
+            iconSrc = "sun2.png"
+        }
+
+        $(element).find(".day").text(day);
+        $(element).find(".day_img").html(`<img width="50px" src="${iconSrc}" alt="">`);
+        $(element).find(".day_temp").text(`${maxTemp}°C / ${minTemp}°C`);
+
+    });
 }
 
-$("#cities").on("change", async function() {
+$("#cities").on("change", async function () {
     let coords = $(this).val().split(",");
     getWeatherData(coords);
 });
